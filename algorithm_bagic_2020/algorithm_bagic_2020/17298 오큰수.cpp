fb92e0df42a7507731bd31ed282d936dcd8f5a77
zmd9220 <31811018+zmd9220@users.noreply.github.com>
2020-04-17 23:13:23 +0900

    오큰수, 오등큰수 문제 추가

---
+++ algorithm_bagic_2020/algorithm_bagic_2020/17298 오큰수.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int F[1000001] = { 0 };
+// N이 100만이므로 시간복잡도 N2이상은 사용하기 힘듬(시간제한1초 초과 가능성이 매우 높음)
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	vector<int> v(N);
+	vector<int> ans(N);
+	stack<int> st;
+	for (int i = 0; i < N; i++)
+	{
+		int A;
+		cin >> A;
+		v[i] = A;
+	}
+
+	st.push(0);
+	for (int i = 1; i < N; i++)
+	{
+		if (st.empty())
+		{
+			st.push(i);
+		}
+		while (!st.empty() && v[st.top()] < v[i])
+		{
+			ans[st.top()] = v[i];
+			st.pop();
+		}
+		st.push(i);
+	}
+
+	while (!st.empty())
+	{
+		ans[st.top()] = -1;
+		st.pop();
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << ans[i] << " ";
+	}
+
+	cout << '\n';
+	return 0;
+}
--- algorithm_bagic_2020/algorithm_bagic_2020/17299 오등큰수.cpp
+++ algorithm_bagic_2020/algorithm_bagic_2020/17299 오등큰수.cpp
@@ -6,15 +6,17 @@ using namespace std;
 
 int F[1000001] = { 0 };
 int an[1000001] = { 0 };
+int rd[1000001] = { 0 };
 // N이 100만이므로 시간복잡도 N2이상은 사용하기 힘듬(시간제한1초 초과 가능성이 매우 높음)
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
+	stack<int> st;
 	queue<int> q;
 	int N;
+	int cnt = 1;
 	cin >> N;
 	for (int i = 0; i < N; i++)
 	{
@@ -23,26 +25,34 @@ int main()
 		an[i] = A;
 		F[A]++;
 	}
+	st.push(0);
+	for (int i = 1; i < N; i++)
+	{
+		if (st.empty())
+		{
+			st.push(i);
+		}
 
+		while (!st.empty() && F[an[st.top()]] < F[an[i]])
+		{
+
+			rd[st.top()] = an[i];
+			st.pop();
+
+		}
+		st.push(i);
+	}
+	while (!st.empty())
+	{
+		int index = st.top();
+		rd[index] = -1;
+		st.pop();
+	}
 	for (int i = 0; i < N; i++)
 	{
-		int now = an[i];
-		bool found = false;
-		for (int j = i+1; j < N; j++)
-		{
-			int next = an[j];
-			if (F[now] < F[next])
-			{
-				cout << an[j] << " ";
-				found = true;
-				break;
-			}
-		}
-		if (!found)
-		{
-			cout << -1 << " ";
-		}
+		cout << rd[i] << " ";
 	}
+
 	/*while (N--)
 	{
 		int A;
