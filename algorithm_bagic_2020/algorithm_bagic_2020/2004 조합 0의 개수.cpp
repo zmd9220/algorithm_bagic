ca3461a7f965a84e0ed036213e117e0ae0d82fc7
zmd9220 <31811018+zmd9220@users.noreply.github.com>
2020-04-20 17:49:52 +0900

    nCm 0의 갯수 문제

---
+++ algorithm_bagic_2020/algorithm_bagic_2020/2004 조합 0의 개수.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long n, m;
+	cin >> n >> m;
+
+	long long cn = 0, cfn = 0;
+
+	for (long long i = 2; i <= n; i *= 2)
+	{
+		cn += n / i;
+	}
+	for (long long i = 2; i <= m; i *= 2)
+	{
+		cn -= m / i;
+	}
+	for (long long i = 2; i <= n-m; i *= 2)
+	{
+		cn -= (n-m) / i;
+	}
+	for (long long i = 5; i <= n; i *= 5)
+	{
+		cfn += n / i;
+	}
+	for (long long i = 5; i <= m; i *= 5)
+	{
+		cfn -= m / i;
+	}
+	for (long long i = 5; i <= n-m; i *= 5)
+	{
+		cfn -= (n-m) / i;
+	}
+
+	cout << min(cn, cfn) << '\n';
+
+	return 0;
+}
+
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//int main() {
+//	long long two = 0, five = 0;
+//	long long n, m;
+//	cin >> n >> m;
+//	for (long long i = 2; i <= n; i *= 2) {
+//		two += n / i;
+//	}
+//	for (long long i = 2; i <= n - m; i *= 2) {
+//		two -= (n - m) / i;
+//	}
+//	for (long long i = 2; i <= m; i *= 2) {
+//		two -= m / i;
+//	}
+//	for (long long i = 5; i <= n; i *= 5) {
+//		five += n / i;
+//	}
+//	for (long long i = 5; i <= n - m; i *= 5) {
+//		five -= (n - m) / i;
+//	}
+//	for (long long i = 5; i <= m; i *= 5) {
+//		five -= m / i;
+//	}
+//	cout << min(two, five) << '\n';
+//	return 0;
+//}
