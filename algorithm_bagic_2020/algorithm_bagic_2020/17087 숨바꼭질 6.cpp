cd7085a93764fcbde5cccea5d7fffbae22011943
zmd9220 <31811018+zmd9220@users.noreply.github.com>
2020-04-23 21:43:36 +0900

    수학1-연습문제 숨바꼭질6

--- algorithm_bagic_2020/algorithm_bagic_2020/17087 숨바꼭질 6.cpp
+++ algorithm_bagic_2020/algorithm_bagic_2020/17087 숨바꼭질 6.cpp
@@ -25,7 +25,7 @@ int main()
 	long long N, S;
 	cin >> N >> S;
 	vector<long long> v(N);
-	long long ans = LLONG_MAX;
+	
 	bool isOne = false;
 	for (long long i = 0; i < N; i++)
 	{
@@ -40,21 +40,58 @@ int main()
 		}
 		v[i] = abs(S - a);
 	}
+	long long ans = v[0];
 	if (isOne == false)
 	{
-		for (long long i = 0; i < N - 1; i++)
+		for (long long i = 1; i < N; i++)
 		{
-			for (long long j = i + 1; j < N; j++)
-			{
-				long long b = GCD(v[i], v[j]);
-				if (ans > b)
-				{
-					ans = b;
-				}
-			}
+			ans = GCD(ans, v[i]);
 		}
+
 		cout << ans << '\n';
 	}
+	//if (isOne == false)
+	//{
+	//	for (long long i = 0; i < N - 1; i++)
+	//	{
+	//		for (long long j = i + 1; j < N; j++)
+	//		{
+	//			long long b = GCD(v[i], v[j]);
+	//			if (ans > b)
+	//			{
+	//				ans = b;
+	//			}
+	//		}
+	//	}
+	//	cout << ans << '\n';
+	//}
 
 	return 0;
 }
+
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int gcd(int x, int y) {
+//	if (y == 0) return x;
+//	else return gcd(y, x%y);
+//}
+//int main() {
+//	int n, s;
+//	cin >> n >> s;
+//	vector<int> a(n);
+//	for (int i = 0; i<n; i++) {
+//		int x;
+//		cin >> x;
+//		int diff = x - s;
+//		if (diff < 0) diff = -diff;
+//		a[i] = diff;
+//	}
+//	int ans = a[0];
+//	for (int i = 1; i<n; i++) {
+//		ans = gcd(ans, a[i]);
+//	}
+//	cout << ans << '\n';
+//	return 0;
+//}
