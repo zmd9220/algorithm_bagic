062ca630cad6d353ed387ffe2f395ed8aac432ba
zmd9220 <31811018+zmd9220@users.noreply.github.com>
2020-05-06 23:41:11 +0900

    dp 파트3조금 심화버전 카드 구매하기

---
+++ algorithm_bagic_2020/algorithm_bagic_2020/11052 카드 구매하기.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	int p[10001] = { 0 };
+	int d[1001] = { 0 };
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> p[i];
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			d[i] = max(d[i], d[i - j] + p[j]);
+		}
+	}
+	cout << d[n] << '\n';
+}
