b627bc0c9740504f2fbada191fe6dacb8dc8ea4d
zmd9220 <31811018+zmd9220@users.noreply.github.com>
2020-05-14 23:02:58 +0900

    dp LIS문제

---
+++ algorithm_bagic_2020/algorithm_bagic_2020/11053 가장 긴 증가하는 부분 수열.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int d[1001] = { 0};
+	int A[1001] = { 0 };
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> A[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		d[i]++;
+		for (int j = i-1; j >= 0; j--)
+		{
+			if (A[i] > A[j])
+			{
+				if (d[j] >= d[i])
+				{
+					d[i] = d[j] + 1;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (d[i] > ans)
+			ans = d[i];
+	}
+	cout << ans << '\n';
+
+}
+
+
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//using namespace std;
+//int main() {
+//	int n;
+//	cin >> n;
+//	vector<int> a(n);
+//	for (int i = 0; i<n; i++) {
+//		cin >> a[i];
+//	}
+//	vector<int> d(n);
+//	for (int i = 0; i<n; i++) {
+//		d[i] = 1;
+//		for (int j = 0; j<i; j++) {
+//			if (a[j] < a[i] && d[j] + 1 > d[i]) {
+//				d[i] = d[j] + 1;
+//			}
+//		}
+//	}
+//
+//	cout << *max_element(d.begin(), d.end()) << '\n';
+//	return 0;
+//}
