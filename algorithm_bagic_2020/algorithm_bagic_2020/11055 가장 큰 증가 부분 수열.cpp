9b85276230d66b34e75292fe3fc79c8384bb82db
zmd9220 <31811018+zmd9220@users.noreply.github.com>
2020-05-31 12:14:22 +0900

    BIS 문제 다시한번 품

--- algorithm_bagic_2020/algorithm_bagic_2020/11055 가장 큰 증가 부분 수열.cpp
+++ algorithm_bagic_2020/algorithm_bagic_2020/11055 가장 큰 증가 부분 수열.cpp
@@ -1,27 +1,75 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
+
 using namespace std;
-int main() {
+
+int a[1001];
+int d[1001];
+int main()
+{
 	int n;
 	cin >> n;
-	vector<int> a(n);
-	for (int i = 0; i<n; i++) {
+	for (int i = 0; i < n; i++)
+	{
 		cin >> a[i];
-	}
-	vector<int> d(n);
-	for (int i = 0; i<n; i++) {
 		d[i] = a[i];
-		for (int j = 0; j<i; j++) {
-			if (a[j] < a[i] && d[j] + a[i] > d[i]) {
+	}
+	//a[0] = d[0];
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (a[i] > a[j] && d[i] < d[j] + a[i])
 				d[i] = d[j] + a[i];
-			}
 		}
 	}
-	int ans = *max_element(d.begin(), d.end());
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (d[i] > ans)
+			ans = d[i];
+	}
 	cout << ans << '\n';
-	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//using namespace std;
+//int main() {
+//	int n;
+//	cin >> n;
+//	vector<int> a(n);
+//	for (int i = 0; i<n; i++) {
+//		cin >> a[i];
+//	}
+//	vector<int> d(n);
+//	for (int i = 0; i<n; i++) {
+//		d[i] = a[i];
+//		for (int j = 0; j<i; j++) {
+//			if (a[j] < a[i] && d[j] + a[i] > d[i]) {
+//				d[i] = d[j] + a[i];
+//			}
+//		}
+//	}
+//	int ans = *max_element(d.begin(), d.end());
+//	cout << ans << '\n';
+//	return 0;
+//}
 
 
 //#include <stdio.h>
